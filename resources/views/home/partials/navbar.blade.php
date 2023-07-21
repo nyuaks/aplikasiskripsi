ae55359595e3d97084b6e692a27878c12a6343bf
aldi musthofa <aldimustofa90@gmail.com>
2023-07-21 10:52:23 +0800

    skripsi mudah

--- resources/views/home/partials/navbar.blade.php
+++ resources/views/home/partials/navbar.blade.php
@@ -1,49 +1,3 @@
-<style>
-/* CSS untuk tampilan desktop */
-.navbar {
-    display: flex;
-    justify-content: flex-end;
-}
-
-.navbar ul {
-    display: flex;
-    list-style: none;
-}
-
-.navbar li {
-    margin-right: 20px;
-}
-
-/* CSS untuk tampilan smartphone */
-@media screen and (max-width: 768px) {
-    .navbar {
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .navbar ul {
-        display: none;
-        flex-direction: column;
-        align-items: center;
-        position: absolute;
-        top: 70px; /* Sesuaikan dengan tinggi navbar Anda */
-        left: 0;
-        right: 0;
-        background-color: #f9f9f9;
-        border-top: 1px solid #ccc;
-    }
-
-    .navbar li {
-        margin: 10px 0;
-    }
-
-    .mobile-nav-toggle {
-        display: block;
-        font-size: 24px;
-        cursor: pointer;
-    }
-}
-</style>
 <!-- ======= Header ======= -->
 <header id="header" class="fixed-top bg-primary">
     <div class="container d-flex align-items-center justify-content-between">
@@ -68,17 +22,26 @@
                 <li><a class="nav-link scrollto" href="/login">Login</a></li>
                 @endguest
             </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
+            <i id="mobile-nav-toggle" class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
-
     </div>
-</header><!-- End Header -->
-<script>
-    const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
-    const navbarUl = document.querySelector('.navbar ul');
+    <script>
+    // Mengambil elemen-elemen yang diperlukan
+    const mobileNavToggle = document.getElementById('mobile-nav-toggle');
+    const navbar = document.getElementById('navbar');
 
+    // Menambahkan event listener saat tombol toggle di klik
     mobileNavToggle.addEventListener('click', function() {
-        navbarUl.classList.toggle('show');
+        // Toggle class 'active' pada elemen navbar
+        navbar.classList.toggle('active');
+    });
+
+    // Menambahkan event listener pada saat window di-resize
+    window.addEventListener('resize', function() {
+        // Jika lebar window lebih besar dari 768px, maka remove class 'active'
+        if (window.innerWidth > 768) {
+            navbar.classList.remove('active');
+        }
     });
 </script>
-
+</header><!-- End Header -->
